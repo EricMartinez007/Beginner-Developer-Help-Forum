5f0012b7fab2092200ffb2842b91f9e4f40c3ba9
Eric Martinez <ericmartinez1007@gmail.com>
2026-03-31 19:27:32 -0500

    Made functions for login and register

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,7 +1,64 @@
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import { getUserByEmail } from "../../services/userService"
+import "./Login.css"
+
 export const Login = () => {
+    const [email, setEmail] = useState("")
+    const navigate = useNavigate()
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+
+        getUserByEmail(email).then((foundUsers) => {
+        if (foundUsers.length === 1) {
+            const user = foundUsers[0]
+            localStorage.setItem(
+                "ohara_user",
+                JSON.stringify({
+                    id: user.id,
+                })
+            )
+
+            navigate("/")
+        } else {
+            window.alert("Invalid login")
+        }
+        })
+    }
+
     return (
-        <div>
-            <h1>Welcome To The Login Page!</h1>    
-        </div>
+        <main className="container-login">
+        <section>
+            <form className="form-login" onSubmit={handleLogin}>
+            <h1>Welcome To The Library of Ohara</h1>
+            <h2>Please sign in</h2>
+            <fieldset>
+                <div className="form-group">
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(evt) => setEmail(evt.target.value)}
+                    className="form-control"
+                    placeholder="Email address"
+                    required
+                    autoFocus
+                />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                <button className="login-btn btn-info" type="submit">
+                    Sign in
+                </button>
+                </div>
+            </fieldset>
+            </form>
+        </section>
+        <section>
+            <Link to="/register">Not a member yet?</Link>
+        </section>
+        </main>
     )
+    
 }
--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,7 +1,100 @@
-export const Register = () => {
-    return (
-        <div>
-            <h1>Welcome to Register Screen</h1>
-        </div>
-    )
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import "./Login.css"
+import { createUser, getUserByEmail } from "../../services/userService"
+
+export const Register = (props) => {
+  const [user, setUser] = useState({
+    email: "",
+    name: "",
+    cohort: 0,
+  })
+  let navigate = useNavigate()
+
+  const registerNewUser = () => {
+    createUser(user).then((createdUser) => {
+      if (createdUser.hasOwnProperty("id")) {
+        localStorage.setItem(
+          "ohara_user",
+          JSON.stringify({
+            id: createdUser.id,
+          })
+        )
+
+        navigate("/")
+      }
+    })
+  }
+
+  const handleRegister = (e) => {
+    e.preventDefault()
+    getUserByEmail(user.email).then((response) => {
+      if (response.length > 0) {
+        // Duplicate email. No good.
+        window.alert("Account with that email address already exists")
+      } else {
+        // Good email, create user.
+        registerNewUser()
+      }
+    })
+  }
+
+  const updateUser = (evt) => {
+    const copy = { ...user }
+    copy[evt.target.id] = evt.target.id === "cohort" ? parseInt(evt.target.value) : evt.target.value
+    setUser(copy)
+  }
+
+  return (
+    <main style={{ textAlign: "center" }}>
+      <form className="form-login" onSubmit={handleRegister}>
+        <h1>Welcome To The Library of Ohara</h1>
+        <h2>Please Register</h2>
+        <fieldset>
+          <div className="form-group">
+            <input
+              onChange={updateUser}
+              type="text"
+              id="name"
+              className="form-control"
+              placeholder="Enter your name"
+              required
+              autoFocus
+            />
+          </div>
+        </fieldset>
+        <fieldset>
+          <div className="form-group">
+            <input
+              onChange={updateUser}
+              type="email"
+              id="email"
+              className="form-control"
+              placeholder="Email address"
+              required
+            />
+          </div>
+        </fieldset>
+        <fieldset>
+          <div className="form-group">
+            <input
+              onChange={updateUser}
+              type="number"
+              id="cohort"
+              className="form-control"
+              placeholder="Cohort number"
+              required
+            />
+          </div>
+        </fieldset>
+        <fieldset>
+          <div className="form-group">
+            <button className="login-btn btn-info" type="submit">
+              Register
+            </button>
+          </div>
+        </fieldset>
+      </form>
+    </main>
+  )
 }
