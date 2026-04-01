a3c197150a5b7b493290005c94d37e51242a5afd
Eric Martinez <ericmartinez1007@gmail.com>
2026-03-31 19:43:13 -0500

    Finished routes set up for testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import { ApplicationView } from "./components/views/ApplicationView"
+import { Login } from "./components/auth/Login"
 import { Register } from "./components/auth/Register"
 
 export const App = () => {
--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -1,7 +1,43 @@
+import { Link, useNavigate } from "react-router-dom"
+
 export const NavBar = () => {
+    const navigate = useNavigate()
+
     return (
-        <div>
-            <h1>Welcome To The My Posts Page!</h1>    
-        </div>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">All Posts</Link>
+                </li>
+                <li>
+                    <Link to="myposts">My Posts</Link>
+                </li>
+                <li>
+                    <Link to="favorites">Favorites</Link>
+                </li>
+                <li>
+                    <Link to="newpost">New Post</Link>
+                </li>
+                <li>
+                    <Link to="profile">Profile</Link>
+                </li>
+                {localStorage.getItem("ohara_user") ? (
+                    <li className="navbar-item navbar-logout">
+                        <Link
+                            className="navbar-link"
+                            to=""
+                            onClick={() => {
+                                localStorage.removeItem("ohara_user")
+                                navigate("/login", { replace: true })
+                            }}
+                        >
+                            Logout
+                        </Link>
+                    </li>
+                ) : (
+                ""
+                )}
+            </ul>
+        </nav>
     )
 }
